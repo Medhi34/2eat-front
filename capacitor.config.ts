79e251506c7323c6d54614589c8ade4af8f8acc6
Medhi <42168278+Medhi34@users.noreply.github.com>
2023-05-07 20:09:15 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'swiggyCloneApp',
+  appName: '2Eat-App',
   webDir: 'www',
   bundledWebRuntime: false
 };
